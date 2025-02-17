76e6c416462a9138ec09dccf9c6cf3a122f431ac
harshkhavale <harshkhavale1102@gmail.com>
2025-02-17 15:22:11 +0530

    feat:prisma init

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["assets.aceternity.com"], // Add Unsplash domain here
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
